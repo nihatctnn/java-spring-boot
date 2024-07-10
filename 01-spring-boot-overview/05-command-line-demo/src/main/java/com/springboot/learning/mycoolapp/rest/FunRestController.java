aeb103b0a43ca5e10a25bc4c2cda1ea996df8cbd
Nihat <nihat0851@gmail.com>
2024-07-10 23:11:14 +0300

    section 1 projects are done

---
+++ 01-spring-boot-overview/05-command-line-demo/src/main/java/com/springboot/learning/mycoolapp/rest/FunRestController.java
@@ -0,0 +1,24 @@
+package com.springboot.learning.mycoolapp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+    
+    @GetMapping("/")
+    public String sayHello(){
+        return "Hello World !";
+    }
+
+    @GetMapping("/workout")
+    public String getDailyWorkout(){
+        return " LET HIM COOK ! I SAID LET HIM COOOK !";
+    }
+
+    @GetMapping("/fortune")
+    public String getDailyFortune(){
+        return "Today is your lucky day :)";
+    }
+
+}
