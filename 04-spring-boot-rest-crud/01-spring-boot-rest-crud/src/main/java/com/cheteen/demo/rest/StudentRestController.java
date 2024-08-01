2eead410cf1caf811a0b7282f332f0295aff4e89
Nihat <nihat0851@gmail.com>
2024-08-01 21:17:41 +0300

    projet is improved

--- 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/cheteen/demo/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/cheteen/demo/rest/StudentRestController.java
@@ -2,26 +2,43 @@ package com.cheteen.demo.rest;
 
 
 import com.cheteen.demo.entity.Student;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.annotation.PostConstruct;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+
 
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
 
-    @GetMapping("/students")
-    public List<Student> getStudents(){
+    private List<Student> theStudents;
 
-        List<Student> theStudents = new ArrayList<>();
+    @PostConstruct
+    public void loadData() {
+
+        theStudents = new ArrayList<>();
 
         theStudents.add(new Student("Nihat", "Çetin"));
         theStudents.add(new Student("Ömer", "Çetin"));
         theStudents.add(new Student("Fatih", "Çetin"));
+    }
 
+    @GetMapping("/students")
+    public List<Student> getStudents() {
         return theStudents;
+    }
+
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId) {
+
+        if ((studentId >= theStudents.size()) || (studentId < 0)) {
+            throw new StudentNotFoundException("Student id not found - " + studentId);
+        }
+
+        return theStudents.get(studentId);
     }
 }
