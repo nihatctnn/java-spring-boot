f25263865f07b3b9727f2c731b2161c25b523b3b
Nihat <nihat0851@gmail.com>
2024-08-17 23:07:06 +0300

    project is added

---
+++ 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/cheteen/demo/controller/HelloWorldController.java
@@ -0,0 +1,19 @@
+package com.cheteen.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+
+    @RequestMapping("/showForm")
+    public String showForm(){
+        return "helloworld-form";
+    }
+
+    @RequestMapping("/processForm")
+    public String processForm(){
+        return "helloworld";
+    }
+
+}
