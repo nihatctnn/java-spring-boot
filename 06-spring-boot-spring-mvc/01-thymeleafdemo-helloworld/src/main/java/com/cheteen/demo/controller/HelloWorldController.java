de9db6bd84c6c2ecebc50767e9b41c867f11434a
Nihat <nihat0851@gmail.com>
2024-08-19 23:56:32 +0300

    project is added

--- 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/cheteen/demo/controller/HelloWorldController.java
+++ 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/cheteen/demo/controller/HelloWorldController.java
@@ -3,12 +3,14 @@ package com.cheteen.demo.controller;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
 
-    @RequestMapping("/showForm")
+    @GetMapping("/showForm")
     public String showForm(){
         return "helloworld-form";
     }
@@ -32,4 +34,15 @@ public class HelloWorldController {
         return "helloworld";
     }
 
+    @GetMapping("/processFormVersionThree")
+    public String processFormVersionThree(@RequestParam("studentName") String theName, Model model) {
+
+        theName = theName.toUpperCase();
+
+        String result = "Hey My Friend from v3 ! " + theName;
+
+        model.addAttribute("message", result);
+
+        return "helloworld";
+    }
 }
