9c7240fbe56a7768fa99c00058c03f9577713ec3
Nihat <nihat0851@gmail.com>
2024-08-22 03:00:40 +0300

    project is added

--- 06-spring-boot-spring-mvc/validationdemo/validationdemo/src/main/java/com/cheteen/springmvc/CustomerController.java
+++ 06-spring-boot-spring-mvc/validationdemo/validationdemo/src/main/java/com/cheteen/springmvc/CustomerController.java
@@ -1,15 +1,25 @@
 package com.cheteen.springmvc;
 
 import jakarta.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CustomerController {
+
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder){
+
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+        dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
 
     @GetMapping("/")
     public String showForm(Model theModel){
@@ -24,6 +34,12 @@ public class CustomerController {
             @Valid @ModelAttribute("customer") Customer theCustomer,
             BindingResult theBindingResult) {
 
+        System.out.println("Last name: |" + theCustomer.getLastName() + "|");
+
+        System.out.println("Binding results: " + theBindingResult.toString());
+
+        System.out.println("\n\n\n\n");
+
         if(theBindingResult.hasErrors()){
             return "customer-form";
         }
@@ -33,5 +49,4 @@ public class CustomerController {
         }
 
     }
-
 }
